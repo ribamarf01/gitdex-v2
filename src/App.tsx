d162d405cbb04fc8635569443b1049282e1bc892
ribamarf01 <ribamar.f01@gmail.com>
2022-07-16 14:05:18 -0300

    darkmode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState, FormEvent, FC } from 'react'
 
+import ThemeSwitcher from './components/themeSwitcher'
+
 interface Dev {
   name: string
   login: string
@@ -35,15 +37,16 @@ const App = () => {
 
   }
 
-  return <div className="flex flex-col items-center gap-y-8 p-8 w-full h-screen bg-gray-300 dark:bg-gray-900 font-pop">
+  return <div className="flex relative flex-col items-center gap-y-8 p-8 w-full h-screen bg-gray-300 dark:bg-gray-900 font-pop transition-colors duration-200">
+    <ThemeSwitcher />
     <div className="flex flex-col items-center gap-y-6">
       <img className="dark:invert h-16" src="/img/github-logo.png" alt="Github Logo" />
       <h1 className="dark:text-white text-5xl font-extrabold">GitDex!</h1>
       <p className="text-2xl dark:text-white">Find your favorite dev!</p>
       <form className='flex flex-row items-center gap-x-4' onSubmit={e => fetchFromApi(e)}>
-        <input className='w-full p-4 text-center dark:text-white bg-transparent border-2 rounded-xl hover:border-gray-400 transition-colors duration-300 outline-none' type="text" placeholder='Dev name' value={name} onChange={(e => setName(e.target.value))} />
-        <button type='submit' className='border-2 p-3 rounded-xl hover:border-gray-400 hover:dark:border-white hover:dark:bg-white transition-colors duration-300 group'>
-          <svg className="dark:text-white w-8 h-8 dark:group-hover:text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        <input className='w-full p-4 text-center dark:text-white bg-transparent border-2 rounded-xl border-gray-500 hover:border-gray-900 hover:dark:border-white transition-colors duration-300 outline-none' type="text" placeholder='Dev name' value={name} onChange={(e => setName(e.target.value))} />
+        <button type='submit' className='border-2 p-3 rounded-xl border-gray-500 hover:border-gray-900 hover:dark:border-white hover:bg-gray-900 hover:dark:bg-white transition-colors duration-300 group'>
+          <svg className="dark:text-white w-8 h-8 group-hover:text-white dark:group-hover:text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
         </button>
       </form>
     </div>
